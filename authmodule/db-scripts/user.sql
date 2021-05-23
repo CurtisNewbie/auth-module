7047899534526f73bddcf9c5ec1f535ab2abbe93
CurtisNewbie <zhuangyongj@gmail.com>
2021-05-23 11:27:28 +0800

    Add DDL script for access_log table

--- authmodule/db-scripts/user.sql
+++ authmodule/db-scripts/user.sql
@@ -3,4 +3,12 @@ CREATE TABLE IF NOT EXISTS user (
     username VARCHAR(32) NOT NULL UNIQUE COMMENT "username (must be unique)",
     password VARCHAR(255) NOT NULL COMMENT "password in hash",
     salt VARCHAR(10) NOT NULL COMMENT "salt"
-) ENGINE=InnoDB;
+) ENGINE=InnoDB COMMENT 'user';
+
+CREATE TABLE IF NOT EXISTS access_log (
+    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT COMMENT 'primary key',
+    access_time TIMESTAMP NOT NULL DEFAULT NOW() COMMENT 'when the user signed in',
+    ip_address VARCHAR(255) NOT NULL COMMENT 'ip address',
+    username VARCHAR(255) NOT NULL COMMENT 'username',
+    user_id INT UNSIGNED NOT NULL COMMENT 'primary key of user'
+) ENGINE=InnoDB COMMENT 'access log';
