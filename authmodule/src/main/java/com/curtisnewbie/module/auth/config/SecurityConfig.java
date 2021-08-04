d1ca07c86956fef05249340927faa6af648a2811
CurtisNewbie <zhuangyongj@gmail.com>
2021-08-04 13:37:12 +0800

    Skip configuration is properties are not found

--- authmodule/src/main/java/com/curtisnewbie/module/auth/config/SecurityConfig.java
+++ authmodule/src/main/java/com/curtisnewbie/module/auth/config/SecurityConfig.java
@@ -39,9 +39,6 @@ import javax.servlet.Filter;
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private static final Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
-    private static final String DEFAULT_LOGIN_PROCESSING_URL = "/login";
-    private static final String DEFAULT_LOGOUT_URL = "/logout";
-    private static final String CUSTOM_LOGIN_PAGE_NOT_SET = "----";
     private static final String EMPTY_STRING = "";
     private static final String VALUE_PERMITTED_ANT_PATTERNS = "authmodule.permitted-ant-patterns";
     private static final String VALUE_LOGIN_PROCESSING_URL = "authmodule.login-processing-url";
@@ -64,31 +61,37 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Value("${" + VALUE_PERMITTED_ANT_PATTERNS + ":" + EMPTY_STRING + "}") // default to "" empty string
     private String[] permittedAntPatterns;
-    @Value("${" + VALUE_LOGIN_PROCESSING_URL + ":" + DEFAULT_LOGIN_PROCESSING_URL + "}")
+    @Value("${" + VALUE_LOGIN_PROCESSING_URL + ":" + EMPTY_STRING + "}")
     private String loginProcessingUrl;
-    @Value("${" + VALUE_CUSTOM_LOGIN_PAGE + ":" + CUSTOM_LOGIN_PAGE_NOT_SET + "}")
+    @Value("${" + VALUE_CUSTOM_LOGIN_PAGE + ":" + EMPTY_STRING + "}")
     private String customLoginPage;
-    @Value("${" + VALUE_LOGOUT_URL + ":" + DEFAULT_LOGOUT_URL + "}")
+    @Value("${" + VALUE_LOGOUT_URL + ":" + EMPTY_STRING + "}")
     private String logoutUrl;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // setup which requests are permitted and which requests require authentication
-        http
-                .authorizeRequests().antMatchers(permittedAntPatterns).permitAll()
-                .anyRequest().authenticated();
-        logger.info("Permit all requests for ant patterns: {}", permittedAntPatterns, toString());
+        if (permittedAntPatterns.length > 0) {
+            http.authorizeRequests().antMatchers(permittedAntPatterns).permitAll();
+            logger.info("Permit all requests for ant patterns: {}", permittedAntPatterns, toString());
+        }
+
+        // any other requests must be authenticated
+        http.authorizeRequests().anyRequest().authenticated();
 
         // setup the processing url, success handle (delegate) and failure handler (delegate)
         http.formLogin()
                 .permitAll()
-                .loginProcessingUrl(loginProcessingUrl)
                 .successHandler(authenticationSuccessHandlerDelegate)
                 .failureHandler(authenticationFailureHandlerDelegate);
-        logger.info("Setup login processing url: {}", loginProcessingUrl);
+
+        if (!loginProcessingUrl.equals(EMPTY_STRING)) {
+            http.formLogin().loginProcessingUrl(loginProcessingUrl);
+            logger.info("Setup login processing url: {}", loginProcessingUrl);
+        }
 
         // if customLoginPage is present, use the custom one instead of the generated one
-        if (!customLoginPage.equals(CUSTOM_LOGIN_PAGE_NOT_SET)) {
+        if (!customLoginPage.equals(EMPTY_STRING)) {
             logger.info("Using custom login page: {}", customLoginPage);
             http.formLogin()
                     .loginPage(customLoginPage);
@@ -97,9 +100,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // setup logoutUrl and logout success handler (delegate)
         http.logout()
                 .permitAll()
-                .logoutUrl(logoutUrl)
                 .logoutSuccessHandler(logoutSuccessHandlerDelegate);
-        logger.info("Setup logout url: {}", logoutUrl);
+        if (!logoutUrl.equals(EMPTY_STRING)) {
+            http.logout().logoutUrl(logoutUrl);
+            logger.info("Setup logout url: {}", logoutUrl);
+        }
 
         http.cors()
                 .disable()
