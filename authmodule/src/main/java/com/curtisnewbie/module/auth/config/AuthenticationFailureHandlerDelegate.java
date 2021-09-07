319123f3871b40e018bf9e5e4aee4aff9aaa9290
CurtisNewbie <zhuangyongj@gmail.com>
2021-09-07 21:35:27 +0800

    Default handler for authentication failure

--- authmodule/src/main/java/com/curtisnewbie/module/auth/config/AuthenticationFailureHandlerDelegate.java
+++ authmodule/src/main/java/com/curtisnewbie/module/auth/config/AuthenticationFailureHandlerDelegate.java
@@ -1,8 +1,12 @@
 package com.curtisnewbie.module.auth.config;
 
+import com.curtisnewbie.common.util.JsonUtils;
+import com.curtisnewbie.common.vo.Result;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
@@ -25,7 +29,7 @@ public class AuthenticationFailureHandlerDelegate implements AuthenticationFailu
     private AuthenticationFailureHandlerExtender extender;
 
     @PostConstruct
-    void postConstruct(){
+    void postConstruct() {
         if (extender != null) {
             logger.info("Detected extender, will invoke {}'s implementation", extender.getClass().getName());
         }
@@ -36,11 +40,20 @@ public class AuthenticationFailureHandlerDelegate implements AuthenticationFailu
             throws IOException, ServletException {
         if (extender != null) {
             extender.onAuthenticationFailure(request, response, exception);
+        } else {
+            defaultHandler(request, response, exception);
         }
     }
 
-    private void defaultHandler(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
-        logger.info("Using Default handler for failed authentication, exception: {}", exception);
-        response.getWriter().write("Incorrect username or password, please try again.");
+    private void defaultHandler(HttpServletRequest request, HttpServletResponse response, AuthenticationException e)
+            throws IOException, ServletException {
+        String errorMsg = "Incorrect credentials";
+        if (e instanceof DisabledException) {
+            errorMsg = "User is disabled";
+        } else if (e instanceof InsufficientAuthenticationException) {
+            errorMsg = "You are not allowed to use this application";
+        }
+        response.getWriter().write(JsonUtils.writeValueAsString(Result.error(errorMsg)));
     }
+
 }
