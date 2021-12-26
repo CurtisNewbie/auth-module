0f0acf6731b8a8daac8400656f822bb798d4066b
CurtisNewbie <zhuangyongj@gmail.com>
2021-12-26 23:18:37 +0800

    Validate user's right to use current app for jwt-based auth

--- authmodule/src/main/java/com/curtisnewbie/module/auth/processing/JwtAuthenticator.java
+++ authmodule/src/main/java/com/curtisnewbie/module/auth/processing/JwtAuthenticator.java
@@ -3,11 +3,16 @@ package com.curtisnewbie.module.auth.processing;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.curtisnewbie.module.auth.config.JwtAuthenticationToken;
+import com.curtisnewbie.module.auth.config.ModuleConfig;
 import com.curtisnewbie.module.jwt.domain.api.JwtDecoder;
+import com.curtisnewbie.service.auth.remote.api.RemoteUserAppService;
 import com.curtisnewbie.service.auth.remote.vo.UserVo;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -28,6 +33,15 @@ public class JwtAuthenticator implements Authenticator {
     @Autowired
     private JwtDecoder jwtDecoder;
 
+    @Autowired
+    private ModuleConfig moduleConfig;
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @DubboReference
+    private RemoteUserAppService remoteUserAppService;
+
     @Override
     public AuthenticationResult authenticate(Authentication auth) throws AuthenticationException {
         final Object credentials = auth.getCredentials();
@@ -44,6 +58,12 @@ public class JwtAuthenticator implements Authenticator {
             Assert.notNull(userVo.getId(), "id == null");
             Assert.notNull(userVo.getUsername(), "username == null");
             Assert.notNull(userVo.getRole(), "role == null");
+
+            if (moduleConfig.isAppAuthorizationChecked()
+                    && !remoteUserAppService.isUserAllowedToUseApp(userVo.getId(), applicationName)) {
+                log.info("User '{}' not allowed to use this application", userVo.getUsername());
+                throw new InsufficientAuthenticationException("User '" + userVo.getUsername() + "' not allowed to use this application");
+            }
 
             JwtAuthenticationToken token = new JwtAuthenticationToken(encoded,
                     userVo,
