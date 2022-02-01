a5bbc7b1040fb70d8da3006b2294a61fa3b4c2dd
CurtisNewbie <zhuangyongj@gmail.com>
2022-02-01 14:42:35 +0800

    Update common-module to 2.0.8

--- auth-module/src/main/java/com/curtisnewbie/module/auth/processing/RemoteAuthenticator.java
+++ auth-module/src/main/java/com/curtisnewbie/module/auth/processing/RemoteAuthenticator.java
@@ -1,20 +1,14 @@
 package com.curtisnewbie.module.auth.processing;
 
+import com.curtisnewbie.common.exceptions.UnrecoverableException;
 import com.curtisnewbie.common.vo.Result;
 import com.curtisnewbie.module.auth.config.ModuleConfig;
-import com.curtisnewbie.service.auth.remote.exception.PasswordIncorrectException;
-import com.curtisnewbie.service.auth.remote.exception.UserDisabledException;
-import com.curtisnewbie.service.auth.remote.exception.UserNotAllowedToUseApplicationException;
-import com.curtisnewbie.service.auth.remote.exception.UsernameNotFoundException;
 import com.curtisnewbie.service.auth.remote.feign.UserServiceFeign;
 import com.curtisnewbie.service.auth.remote.vo.LoginVo;
 import com.curtisnewbie.service.auth.remote.vo.UserVo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -60,23 +54,15 @@ public class RemoteAuthenticator implements Authenticator {
             else
                 userResult = remoteUserService.login(loginVo);
 
+            // throw exception if notOk
             userResult.assertIsOk();
 
             log.info("User '{}' authenticated", username);
             return buildSuccessfulAuthentication(userResult.getData(), auth);
 
-        } catch (UserDisabledException e) {
-            log.info("User '{}' is disabled", username);
-            throw new DisabledException("User '" + username + "' is disabled");
-        } catch (UsernameNotFoundException e) {
-            log.info("User '{}' not found", username);
-            throw new org.springframework.security.core.userdetails.UsernameNotFoundException("User '" + username + "' not found");
-        } catch (PasswordIncorrectException e) {
-            log.info("Incorrect password for user '{}'", username);
-            throw new BadCredentialsException("Incorrect username or password");
-        } catch (UserNotAllowedToUseApplicationException e) {
-            log.info("User '{}' not allowed to use this application", username);
-            throw new InsufficientAuthenticationException("User '" + username + "' not allowed to use this application");
+        } catch (Exception e) {
+            // todo this won't happen, but we keep this before we change auth-service-remote
+            throw new UnrecoverableException(e.getMessage(), e);
         }
     }
 
